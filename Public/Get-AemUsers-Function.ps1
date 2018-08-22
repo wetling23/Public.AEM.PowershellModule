5539ecf1f7df959803558df300c7ef63005a5b66
Mike Hashemi <mhashemi@synoptek.com>
2018-08-22 07:28:56 -0600

    Added file.

---
+++ Public/Get-AemUsers-Function.ps1
@@ -0,0 +1,109 @@
+﻿Function Get-AemUsers {
+    <#
+        .DESCRIPTION
+            Retrieves all users from AutoTask Endpoint Management.
+        .NOTES 
+            Author: Mike Hashemi
+            V1.0.0.0 date: 22 August 2018
+                - Initial release.
+        .PARAMETER AemAccessToken
+            Mandatory parameter. Represents the token returned once successful authentication to the API is achieved. Use New-AemApiAccessToken to obtain the token.
+        .PARAMETER ApiUrl
+            Default value is 'https://zinfandel-api.centrastage.net'. Represents the URL to AutoTask's AEM API, for the desired instance.
+        .PARAMETER EventLogSource
+            Default value is 'AemPowerShellModule'. This parameter is used to specify the event source, that script/modules will use for logging.
+        .PARAMETER BlockLogging
+            When this switch is included, the code will write output only to the host and will not attempt to write to the Event Log.
+        .EXAMPLE
+            .\Get-AemUsers -AemAccessToken <bearer token>
+
+            This example returns an array of all AEM users and their properties.
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True, ValueFromPipeline = $true)]
+        [string]$AemAccessToken,
+
+        [string]$ApiUrl = 'https://zinfandel-api.centrastage.net',
+
+        [string]$EventLogSource = 'AemPowerShellModule',
+
+        [switch]$BlockLogging
+    )
+
+    Begin {
+        If (-NOT($BlockLogging)) {
+            $return = Add-EventLogSource -EventLogSource $EventLogSource
+
+            If ($return -ne "Success") {
+                $message = ("{0}: Unable to add event source ({1}). No logging will be performed." -f (Get-Date -Format s), $EventLogSource)
+                Write-Host $message -ForegroundColor Yellow;
+
+                $BlockLogging = $True
+            }
+        }
+
+        $message = ("{0}: Beginning {1}." -f (Get-Date -Format s), $MyInvocation.MyCommand)
+        If (($BlockLogging) -AND ($PSBoundParameters['Verbose'])) {Write-Verbose $message} ElseIf ($PSBoundParameters['Verbose']) {Write-Verbose $message; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Information -Message $message -EventId 5417}
+    }
+
+    Process {
+        # Define parameters for Invoke-WebRequest cmdlet.
+        $params = @{
+            Uri         = '{0}/api{1}' -f $ApiUrl, "/v2/account/users"
+            Method      = 'GET'
+            ContentType = 'application/json'
+            Headers     = @{'Authorization' = 'Bearer {0}' -f $AemAccessToken}
+        }
+
+        # Make request.
+        $message = ("{0}: Making the first web request." -f (Get-Date -Format s), $MyInvocation.MyCommand)
+        If (($BlockLogging) -AND ($PSBoundParameters['Verbose'])) {Write-Verbose $message} ElseIf ($PSBoundParameters['Verbose']) {Write-Verbose $message; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Information -Message $message -EventId 5417}
+
+        Try {
+            $webResponse = (Invoke-WebRequest @params -ErrorAction Stop).Content
+        }
+        Catch {
+            $message = ("{0}: It appears that the web request failed. Check your credentials and try again. To prevent errors, {1} will exit. The specific error message is: {2}" `
+                    -f (Get-Date -Format s), $MyInvocation.MyCommand, $_.Exception.Message)
+            If ($BlockLogging) {Write-Host $message -ForegroundColor Red} Else {Write-Host $message -ForegroundColor Red; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Error -Message $message -EventId 5417}
+
+            Return "Error"
+        }
+
+        $users = ($webResponse | ConvertFrom-Json).users
+
+        While ((($webResponse | ConvertFrom-Json).pageDetails).nextPageUrl) {
+            $page = ((($webResponse | ConvertFrom-Json).pageDetails).nextPageUrl).Split("&")[1]
+            $resourcePath = "/v2/account/users?$page"
+
+            $params = @{
+                Uri         = '{0}/api{1}' -f $ApiUrl, $resourcePath
+                Method      = 'GET'
+                ContentType = 'application/json'
+                Headers     = @{'Authorization'	= 'Bearer {0}' -f $AemAccessToken}
+            }
+
+            $message = ("{0}: Making web request for page {1}." -f (Get-Date -Format s), $page.TrimStart("page="))
+            If (($BlockLogging) -AND ($PSBoundParameters['Verbose'])) {Write-Verbose $message} ElseIf ($PSBoundParameters['Verbose']) {Write-Verbose $message; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Information -Message $message -EventId 5417}
+
+            Try {
+                $webResponse = (Invoke-WebRequest @params).Content
+            }
+            Catch {
+                $message = ("{0}: It appears that the web request failed. Check your credentials and try again. To prevent errors, {1} will exit. The specific error message is: {2}" `
+                        -f (Get-Date -Format s), $MyInvocation.MyCommand, $_.Exception.Message)
+                If ($BlockLogging) {Write-Host $message -ForegroundColor Red} Else {Write-Host $message -ForegroundColor Red; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Error -Message $message -EventId 5417}
+
+                Return "Error"
+            }
+
+            $message = ("{0}: Retrieved an additional {1} devices." -f (Get-Date -Format s), (($webResponse|ConvertFrom-Json).devices).count)
+            If (($BlockLogging) -AND ($PSBoundParameters['Verbose'])) {Write-Verbose $message} ElseIf ($PSBoundParameters['Verbose']) {Write-Verbose $message; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Information -Message $message -EventId 5417}
+            
+            $users += ($webResponse|ConvertFrom-Json).users
+        }
+
+        Return $users
+    }
+} #1.0.0.0
