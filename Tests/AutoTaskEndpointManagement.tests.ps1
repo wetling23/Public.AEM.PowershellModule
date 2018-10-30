88cb0bf2e8fad5354b426a6197ce0aec6c1a7654
Konstantin Kaminskiy <konstantin.kaminskiy@outlook.com>
2018-10-29 20:32:53 -0400

    added test for -UseBasicParsing

---
+++ Tests/AutoTaskEndpointManagement.tests.ps1
@@ -0,0 +1,16 @@
+Get-Module AutoTaskEndpointManagement | Remove-Module -ErrorAction SilentlyContinue
+Import-Module $PSScriptRoot\..\bin\1.0.0.4\AutoTaskEndpointManagement\AutoTaskEndpointManagement.psd1
+. $PSScriptRoot\helpertest.ps1
+
+Describe 'Testing functions for use of -UseBasicParsing' {
+    $functions = Get-Command -Module AutoTaskEndpointManagement | Select-Object -ExpandProperty Name
+    foreach ($function in $functions) {
+        It "$function has the same count of Invoke-WebRequest as -UseBasicParsing" {
+            $functioncode = Get-Content Function:\$function
+            $functioncode = Remove-CommentsAndWhiteSpace -Scriptblock $functioncode
+            $CountInvoke = ([regex]::Matches($functioncode,"Invoke-WebRequest")).count
+            $CountUseBasic = ([regex]::Matches($functioncode,"-UseBasicParsing")).count
+            ($CountInvoke) -eq ($CountUseBasic) | Should -be $true
+        }
+    }
+}
